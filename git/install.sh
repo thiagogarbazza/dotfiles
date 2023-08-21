d1ce41c0d728abaa28c0756c1919b50775f4df72
Thiago Garbazza <thiagogarbazza@gmail.com>
2023-08-21 15:13:31 -0300

    feat: Install and configuration of git

--- git/install.sh
+++ git/install.sh
@@ -10,5 +10,7 @@ echo "Install git"
 sudo apt install git -y
 
 echo "Install dotfiles"
+mkdir -p $GIT_CONFIG_HOME
+rm -rf $HOME/.gitconfig
 curl -fsSL https://raw.githubusercontent.com/thiagogarbazza/dotfiles/main/git/gitconfig -o $GIT_CONFIG_HOME/gitconfig
 ln -s $GIT_CONFIG_HOME/gitconfig $HOME/.gitconfig
