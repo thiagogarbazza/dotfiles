223db718f9c71c1b516a29d7688dd1e1da90cb74
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2023-08-28 17:44:21 -0300

    fix: install dev-kit java

--- dev-kit-java/install.sh
+++ dev-kit-java/install.sh
@@ -14,13 +14,10 @@ echo "Install sdkman"
 ## For more information see https://sdkman.io/install
 curl -s "https://get.sdkman.io?rcupdate=false" | bash
 
-. "${SDKMAN_DIR}/bin/sdkman-init.sh"
+source "${SDKMAN_DIR}/bin/sdkman-init.sh"
 
 echo "Install jdk"
-sdk install java 8.0.382-tem
 sdk install java 20.0.2-tem
-
-sdk default java 20.0.2-tem
 
 echo "Install maven"
 sdk install maven
