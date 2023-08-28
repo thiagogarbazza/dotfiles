4095c9761229b16c5dbec672ee5b42a09ab4eb26
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2023-08-28 07:41:42 -0300

    fix(dev-kit-java): source not found on config sdkman

--- dev-kit-java/install.sh
+++ dev-kit-java/install.sh
@@ -14,7 +14,7 @@ echo "Install sdkman"
 ## For more information see https://sdkman.io/install
 curl -s "https://get.sdkman.io?rcupdate=false" | bash
 
-source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+. "${SDKMAN_DIR}/bin/sdkman-init.sh"
 
 echo "Install jdk"
 sdk install java 8.0.382-tem
