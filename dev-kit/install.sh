cd8e0e179d2ad3186df7ab6c551b2f2931b6126b
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2024-04-01 07:23:19 -0300

    feat: add theme dracula for gnome

--- dev-kit/install.sh
+++ dev-kit/install.sh
@@ -13,6 +13,6 @@ echo "Install font jetbrains-mono"
 FONT_VERSION=$(curl -s "https://api.github.com/repos/JetBrains/JetBrainsMono/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 curl -sSLo jetbrains-mono.zip https://download.jetbrains.com/fonts/JetBrainsMono-$FONT_VERSION.zip
 unzip -qq jetbrains-mono.zip -d jetbrains-mono
-sudo mkdir /usr/share/fonts/truetype/jetbrains-mono
+sudo mkdir -p /usr/share/fonts/truetype/jetbrains-mono
 sudo mv jetbrains-mono/fonts/ttf/*.ttf /usr/share/fonts/truetype/jetbrains-mono
 rm -rf jetbrains-mono.zip jetbrains-mono
