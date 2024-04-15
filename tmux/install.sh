48719e2e51cdf809c33fd0a1cbd5cb53fb30b1fb
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2024-04-15 16:36:11 -0300

    feat: adapting install and configure tmux on macOS

--- tmux/install.sh
+++ tmux/install.sh
@@ -9,13 +9,32 @@ echo "
 └──────────────────────────────────────────────────────────────────────────────┘
 "
 
-echo "Install tmux"
-mkdir -p $TMUX_CONFIG_HOME
-if [ -x "$(command -v apt)" ]; then
-  sudo apt install tmux -y
-elif [ -x "$(command -v pacman)" ]; then
-  sudo pacman tmux --sync --noconfirm
+if [ -x "$(command -v tmux)" ]; then
+  echo "tmux already installed"
+else
+  echo "Install tmux"
+
+  case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
+    *darwin*)
+      brew install tmux
+      ;;
+    *linux*)
+      if [ -x "$(command -v apt)" ]; then
+        sudo apt install tmux -y
+      elif [ -x "$(command -v pacman)" ]; then
+        sudo pacman tmux --sync --noconfirm
+      fi
+      ;;
+    *)
+      echo "error: unsupported platform.";
+      exit 2;
+      ;;
+  esac;
 fi
+
+echo "Clean old configuration"
+rm -rf $HOME/.tmux.conf
+rm -rf $TMUX_CONFIG_HOME
 
 echo "Install plugins"
 git clone --depth=1 https://github.com/tmux-plugins/tpm $TMUX_CONFIG_HOME/plugins/tpm
