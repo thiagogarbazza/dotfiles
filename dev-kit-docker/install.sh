498f083a7bf0749f4182fdafe11478e7cfe5cc3e
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2024-04-15 16:33:06 -0300

    feat: adapting install and configure dev-kit-docker on macOS

--- dev-kit-docker/install.sh
+++ dev-kit-docker/install.sh
@@ -7,5 +7,24 @@ echo "
 └──────────────────────────────────────────────────────────────────────────────┘
 "
 
-curl -fsSL https://get.docker.com | bash
+if [ -x "$(command -v docker)" ]; then
+  echo "Docker already installed"
+else
+  echo "Install docker"
+
+  case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
+    *darwin*)
+      brew install docker
+      ;;
+    *linux*)
+      curl -fsSL https://get.docker.com | bash
+      ;;
+    *)
+      echo "error: unsupported platform.";
+      exit 2;
+      ;;
+  esac;
+fi
+
+echo "User configuration"
 sudo usermod -aG docker $(whoami)
