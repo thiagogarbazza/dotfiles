7f2618ffd95c014d028839789a85f2e7c5942db5
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2024-04-15 16:41:19 -0300

    feat: adapting install and configure on macOS

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ export XDG_STATE_HOME=$HOME/.local/state
 echo "Create directories"
 mkdir -p \
   $XDG_CONFIG_HOME \
-  $XDG_CACHE_HOM \
+  $XDG_CACHE_HOME \
   $XDG_DATA_HOME \
   $XDG_STATE_HOME \
-  $HOME/projects/pessoal \
+  $HOME/projects/my \
   $HOME/projects/pocs
 
 echo "
@@ -21,14 +21,22 @@ echo "
 └──────────────────────────────────────────────────────────────────────────────┘
 "
 
-if [ -x "$(command -v apt)" ]; then
-  sudo apt install zip unzip curl wget -y
-elif [ -x "$(command -v pacman)" ]; then
-  sudo pacman zip unzip curl wget --sync --noconfirm
-else
-  echo "Package manager not found"
-  exit 99
-fi
+case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
+  *darwin*)
+    brew install zip unzip curl wget
+    ;;
+  *linux*)
+    if [ -x "$(command -v apt)" ]; then
+      sudo apt install zip unzip curl wget -y
+    elif [ -x "$(command -v pacman)" ]; then
+      sudo pacman zip unzip curl wget --sync --noconfirm
+    fi
+    ;;
+  *)
+    echo "error: unsupported platform.";
+    exit 2;
+    ;;
+esac;
 
 curl -fsSL https://raw.githubusercontent.com/thiagogarbazza/dotfiles/main/git/install.sh | bash
 curl -fsSL https://raw.githubusercontent.com/thiagogarbazza/dotfiles/main/dev-kit/install.sh | bash
