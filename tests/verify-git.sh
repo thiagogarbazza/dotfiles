f643ae30029b4e092b1f1b7950f932ecf1805d94
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2025-05-26 15:44:17 -0300

    fix: improve git verification messages in CI workflow

--- tests/verify-git.sh
+++ tests/verify-git.sh
@@ -1,4 +1,4 @@
 set -euo pipefail
 
-[[ -x "$(command -v git)" ]] || { echo "[git] not installed."; exit 1; }
-[[ "$(git config --global user.name)" == "Thiago Garbazza" ]] || { echo "[git] configuration is incorrect."; exit 1; }
+[[ -x "$(command -v git)" ]] && { echo "[git] installed successfully"; } || { echo "[git] not installed."; exit 1; }
+[[ "$(git config --global user.name)" == "Thiago Garbazza" ]] && { echo "[git] configured successfully"; } || { echo "[git] configuration is incorrect."; exit 1; }
