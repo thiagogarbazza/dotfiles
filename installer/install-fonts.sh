56c6e85740f631f45725b038218737ea6c395e97
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2025-07-31 14:23:05 -0300

    refactor: improve curl command usage in font installation script with enhanced error handling and output messages

--- installer/install-fonts.sh
+++ installer/install-fonts.sh
@@ -26,7 +26,10 @@ echo "
 rm -rf /tmp/cascadia-code.zip /tmp/cascadia-code
 find $DIR_FONT -name "CascadiaCode*" -exec rm -rf {} \;
 FONT_VERSION=$(curl -s "https://api.github.com/repos/microsoft/cascadia-code/releases/latest" | sed -n 's|.*"tag_name": "v\([^"]*\)".*|\1|p')
-curl --location --output /tmp/cascadia-code.zip https://github.com/microsoft/cascadia-code/releases/download/v$FONT_VERSION/CascadiaCode-$FONT_VERSION.zip
+curl --location --show-error \
+  --write-out "Download of %{url} finished\n" \
+  --output /tmp/cascadia-code.zip \
+  https://github.com/microsoft/cascadia-code/releases/download/v$FONT_VERSION/CascadiaCode-$FONT_VERSION.zip
 unzip -qq /tmp/cascadia-code.zip -d /tmp/cascadia-code
 mv /tmp/cascadia-code/ttf/*.ttf $DIR_FONT
 
@@ -39,7 +42,10 @@ echo "
 rm -rf /tmp/firacode.zip /tmp/firacode
 find $DIR_FONT -name "FiraCode*" -exec rm -rf {} \;
 FONT_VERSION=$(curl -s "https://api.github.com/repos/tonsky/FiraCode/releases/latest" | sed -n 's|.*"tag_name": "\([^"]*\)".*|\1|p')
-curl --location --output  /tmp/firacode.zip https://github.com/tonsky/FiraCode/releases/download/$FONT_VERSION/Fira_Code_v$FONT_VERSION.zip
+curl --location --show-error \
+  --write-out "Download of %{url} finished\n" \
+  --output  /tmp/firacode.zip \
+  https://github.com/tonsky/FiraCode/releases/download/$FONT_VERSION/Fira_Code_v$FONT_VERSION.zip
 unzip -qq /tmp/firacode.zip -d /tmp/firacode
 mv /tmp/firacode/ttf/*.ttf $DIR_FONT
 
@@ -52,7 +58,10 @@ echo "
 rm -rf /tmp/hack.zip /tmp/hack
 find $DIR_FONT -name "Hack*" -exec rm -rf {} \;
 FONT_VERSION=$(curl -s "https://api.github.com/repos/source-foundry/Hack/releases/latest" | sed -n 's|.*"tag_name": "\([^"]*\)".*|\1|p')
-curl --location --output  /tmp/hack.zip https://github.com/source-foundry/Hack/releases/download/$FONT_VERSION/Hack-$FONT_VERSION-ttf.zip
+curl --location --show-error \
+  --write-out "Download of %{url} finished %{errormsg}\n" \
+  --output  /tmp/hack.zip \
+  https://github.com/source-foundry/Hack/releases/download/$FONT_VERSION/Hack-$FONT_VERSION-ttf.zip
 unzip -qq /tmp/hack.zip -d /tmp/hack
 mv /tmp/hack/ttf/*.ttf $DIR_FONT
 
@@ -65,6 +74,9 @@ echo "
 rm -rf /tmp/jetbrains-mono.zip /tmp/jetbrains-mono
 find $DIR_FONT -name "JetBrain*" -exec rm -rf {} \;
 FONT_VERSION=$(curl -s "https://api.github.com/repos/JetBrains/JetBrainsMono/releases/latest" | sed -n 's|.*"tag_name": "v\([^"]*\)".*|\1|p')
-curl --location --output /tmp/jetbrains-mono.zip https://download.jetbrains.com/fonts/JetBrainsMono-$FONT_VERSION.zip
+curl --location --show-error \
+  --write-out "Download of %{url} finished\n" \
+  --output /tmp/jetbrains-mono.zip \
+  https://download.jetbrains.com/fonts/JetBrainsMono-$FONT_VERSION.zip
 unzip -qq /tmp/jetbrains-mono.zip -d /tmp/jetbrains-mono
 mv /tmp/jetbrains-mono/fonts/ttf/*.ttf $DIR_FONT
